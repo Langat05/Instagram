46f8c5a81e9f96ad0daac15f7008c7d4a67f5c35
langat justus <justuslangat78@gmail.com>
2020-10-20 10:07:25 +0300

    created the image class

--- langat/models.py
+++ langat/models.py
@@ -17,3 +17,23 @@ class Profile(models.Model):
      #delete profile
     def delete_profile(self):
         self.delete()
+
+class Image(models.Model):
+    time_created= models.DateTimeField(default=datetime.now, blank=True)
+    image=models.ImageField(upload_to='images/')
+    message = models.CharField(max_length=80, blank=True)
+    name = models.CharField(max_length=80)
+    caption = models.TextField(blank=True)
+    profile = models.ForeignKey(User, blank=True,on_delete=models.CASCADE)
+
+
+    def __str__(self):
+        return self.name
+
+    #Save image
+    def save_image(self):
+        self.save()
+
+    #Delete image
+    def delete_image(self):
+        self.delete()        
