e1ae92643a22c9539de6d0d331052dec895220a1
langat justus <justuslangat78@gmail.com>
2020-10-20 11:22:15 +0300

    created profile.html

--- instagram/urls.py
+++ instagram/urls.py
@@ -14,9 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import url, include
+from django.contrib.auth import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('instagram.urls'))
+    url('admin/', admin.site.urls),
+    url('', include('instagram.urls')),
+    url('accounts/', include('registration.backends.simple.urls')),
+    url('logout/', views.logout,{"next_page":'/'}),
 ]
--- langat/urls.py
+++ langat/urls.py
@@ -1,11 +1,11 @@
-from django.conf.urls import path
+from django.conf.urls import url
 from django.conf import settings
 from django.conf.urls.static import static
 from . import views
 
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    
-
+    url('', views.home, name='home'),
+    url(r'^showprofile/(?P<id>\d+)',views.display_profile,name = 'showprofile'),
+ 
 ]
