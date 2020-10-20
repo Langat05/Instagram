a716a707d9cd22a58b0ffa58dda3400360f2e9d0
langat justus <justuslangat78@gmail.com>
2020-10-20 11:32:35 +0300

    added a function to search for usernames

--- langat/views.py
+++ langat/views.py
@@ -9,3 +9,11 @@ def home(request):
     likes = Likes.objects.all
     profile = Profile.objects.all()
     return render(request,'index.html',locals())
+
+def search(request):
+    profiles = User.objects.all()
+    if 'username' in request.GET and request.GET['username']:
+        search_term = request.GET.get('username')
+        results = User.objects.filter(username__icontains=search_term)
+        return render(request,'search.html',locals())
+    return redirect(home)    
