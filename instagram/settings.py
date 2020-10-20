526026213c7bd309e9a277f58e699de862b5f228
langat justus <justuslangat78@gmail.com>
2020-10-20 16:13:58 +0300

    installed the django-friendship

--- instagram/settings.py
+++ instagram/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'langat',
     'bootstrap4',
     'pyuploadcare',
+    'friendship',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
