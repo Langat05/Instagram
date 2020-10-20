5eecd331204b6f0954c7395a54ec42cd8611e249
langat justus <justuslangat78@gmail.com>
2020-10-20 19:56:59 +0300

    fixed the test to pass

--- langat/tests.py
+++ langat/tests.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 from django.contrib.auth.models import User
-from .models Image, Profile, Comment
+from .models import Image, Profile, Comment
 
 
 # Create your tests here.
@@ -10,10 +10,12 @@ class ProfileTescase(TestCase):
     @classmethod
     def setUpTestData(cls):
         cls.user = User.objects.create_user('langat')
-        cls.profile = Profile(profile_pic='', bio ='Brought up in Kenya',cls.user = user)
+        cls.profile = Profile(profile_pic='', bio ='Brought up in Kenya',user=cls.user)
 
     def test_instance(cls):
         cls.assertTrue(isinstance(cls.profile, Profile))        
 
     def save_method_test(self):
         self.profile.save_profile()
+        images = Image.objects.all()
+        self.assertTrue(len(images)> 0)
