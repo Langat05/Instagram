58fbc2620ece5f6993b68567a8d2f217cea70f81
langat justus <justuslangat78@gmail.com>
2020-10-20 12:13:07 +0300

    updated the form.py file

--- langat/forms.py
+++ langat/forms.py
@@ -1,11 +1,25 @@
-from django import forms 
-from .models import Image, Profile
-from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.models import User
+from django import forms
+from .models import Profile,Image,Comment
 
-class SignUpForm(forms.Form):
-    email = forms.EmailField(max_length=50, help_text="Required")
+
+class ProfileForm(forms.ModelForm):
     class Meta:
-        model = User
-        fields = ('username', 'email', 'password', 'password2')
+        model = Profile
+        exclude=['user']
 
+
+class ImageForm(forms.ModelForm):
+    class Meta:
+        model = Image
+        exclude =['likes','profile']
+
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        exclude = ['picture', 'comment_title']
+
+
+class MessageForm(forms.Form):
+    subject = forms.CharField(max_length=100)
+    To = forms.CharField(max_length=100)
