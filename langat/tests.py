824fca563cde39ce5297aa1077e1b5f8393138e7
langat justus <justuslangat78@gmail.com>
2020-10-20 19:50:24 +0300

    fixed the identation error

--- langat/tests.py
+++ langat/tests.py
@@ -12,9 +12,8 @@ class ProfileTescase(TestCase):
         cls.user = User.objects.create_user('langat')
         cls.profile = Profile(profile_pic='', bio ='Brought up in Kenya',cls.user = user)
 
-def test_instance(cls):
-    cls.assertTrue(isinstance(cls.profile, Profile))        
+    def test_instance(cls):
+        cls.assertTrue(isinstance(cls.profile, Profile))        
 
-def save_method_test(self):
-    self.profile.save_profile()
-        
+    def save_method_test(self):
+        self.profile.save_profile()
